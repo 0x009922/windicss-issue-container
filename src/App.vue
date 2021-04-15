752cd7e21316735348e4f254ec410f17eb6bbdf3
0x009922 <a.marcius26@gmail.com>
2021-04-15 05:57:45 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container border">
+    Hello, resize window horizontally and you will see, that there no any horizontal padding
+    <br>
+    Also, there are no centering :/
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ tailwindcss.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite-plugin-windicss'
+
+export default defineConfig({
+    theme: {
+        container: {
+            padding: "30px",
+            center: true,
+        },
+    }
+})
